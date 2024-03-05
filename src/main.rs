3c1eb3f8477fcec90039644783858c9a6b9fc810
Kaustubh Dwivedi <kaustubh.dwivedi@ivanti.com>
2024-03-05 23:36:52 +0530

    commit2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::error::Error;
+use std::{error::Error, io::stdout, time::Duration};
+use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 use rusty_audio::Audio;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -10,7 +11,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("startup", "audio/startup.wav");
     audio.play("startup");
 
+    // terminal
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // game loop
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    },
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
 
