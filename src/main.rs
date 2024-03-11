f0882d56ae20cd7bf403fbc65b594cf75a409aad
Kaustubh Dwivedi <kaustubh.dwivedi@ivanti.com>
2024-03-12 00:49:34 +0530

    rendering frames and multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::{error::Error, io::stdout, time::Duration};
+use std::{error::Error, io::{self, stdout}, sync::mpsc::{self, Receiver}, thread, time::Duration};
 use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
+use invaders::{frame::{self, new_frame, Frame}, render};
 use rusty_audio::Audio;
+
+fn render_screen(render_rx: Receiver<Frame>) {
+    let mut last_frame = frame::new_frame();
+    let mut stdout = io::stdout();
+    render::render(&mut stdout, &last_frame, &last_frame, true);
+    while let Ok(curr_frame) = render_rx.recv() {
+        render::render(&mut stdout, &last_frame, &curr_frame, false);
+        last_frame = curr_frame;
+    }
+}
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
     audio.add("explode", "audio/explode.wav");
@@ -17,8 +28,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+   // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        render_screen(render_rx);
+    });
+
     // game loop
     'gameloop: loop {
+        let curr_frame = new_frame();
+
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
@@ -30,9 +49,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+        
+        // Draw and render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
