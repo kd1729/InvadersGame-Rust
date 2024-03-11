5e1389b48389e51bf131f1969d0dec4c43b04b07
Kaustubh Dwivedi <kaustubh.dwivedi@ivanti.com>
2024-03-12 01:02:16 +0530

    implementaion of player

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, io::{self, stdout}, sync::mpsc::{self, Receiver}, thread, time::Duration};
 use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
-use invaders::{frame::{self, new_frame, Frame}, render};
+use invaders::{frame::{self, new_frame, Drawable, Frame}, player::Player, render};
 use rusty_audio::Audio;
 
 fn render_screen(render_rx: Receiver<Frame>) {
@@ -35,12 +35,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // game loop
+    let mut player = Player::new();
+
     'gameloop: loop {
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -51,6 +55,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         
         // Draw and render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
